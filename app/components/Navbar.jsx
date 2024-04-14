e18a8b369775e275ae2de6c21cfa571677a56e3f
NemesisUA <display.n0ne.01.gmail.com>
2024-04-14 13:57:02 +0300

    create fake API, fetch data in server component

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,16 @@
 import Link from "next/link"
+import Image from "next/image"
+import Logo from "./cutebear.png"
 
 const Navbar = () => {
   return (
     <nav>
+      <Image
+        src={Logo}
+        width={70}
+        placeholder='blur'
+        quality={100}
+      />
       <h1>HelpDesk</h1>
       <Link href='/'>Dashboard</Link>
       <Link href='/tickets'>Tickets</Link>
---
+++ app/tickets/TicketsList.jsx
@@ -0,0 +1,35 @@
+const getTickets = async () => {
+  const res = await fetch('http://localhost:4000/tickets', {
+    next: {
+      revalidate: 0 // when we set to 0, we opt out of using cache
+    }
+  })
+
+  // we return a promice, so TicketsList must be async func
+  // and we can make it async as it is a server component
+  return res.json();
+}
+
+
+const TicketsList = async () => {
+  const tickets = await getTickets();
+
+  return (
+    <>
+      {tickets.map((ticket) => (
+        <div key={ticket.id} className="card my-5">
+          <h3>{ticket.title}</h3>
+          <p>{ticket.body.slice(0, 200)}...</p>
+          <div className={`pill ${ticket.priority}`}>
+            {ticket.priority} priority
+          </div>
+        </div>
+      ))}
+      {tickets.length === 0 && (
+        <p className="text-center">There are no open tickets.</p>
+      )}
+    </>
+  )
+}
+
+export default TicketsList
